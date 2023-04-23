f095c75d65f2b5a1e4f00c332e8df181dc10b219
Ayo <heyayomideadebisi@gmail.com>
2023-04-23 20:12:35 +0100

    solve mobile icon issue

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,21 +1,24 @@
-// import MenuIcon from '@mui/icons-material/Menu';
-// import PersonIcon from '@mui/icons-material/Person';
-// import HelpIcon from '@mui/icons-material/Help';
-// import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-// import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import MenuIcon from "@mui/icons-material/Menu";
+import PersonIcon from "@mui/icons-material/Person";
+import HelpIcon from "@mui/icons-material/Help";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const Header = () => {
   return (
     <div className="bg-white shadow-lg sticky top-0  ">
-      <div className="flex flex-col sm:flex-row sm:justify-between  sm:items-center max-w-[72rem] py-2 m-auto">
-        <div className="md:flex items-center ">
-          <p className="text-lg md:text-3xl md:py-2 px-4 font-ptsans font-bold ">
+      <div className="flex flex-col sm:flex-row sm:justify-between  sm:items-center max-w-[72rem] py-2 px-4 m-auto">
+        <div className="flex items-center justify-between ">
+          <p className="text-lg md:text-3xl md:py-2  font-ptsans font-bold ">
             Shop-It
           </p>
+          <div className="flex sm:hidden ">
+            <PersonIcon className=" " />
+          </div>
         </div>
 
         <div className="flex items-center  gap-x-4 w-full md:w-4/5 ">
-          <div className=" w-full flex items-center h-10 gap-2 mx-2 ">
+          <div className=" w-full flex items-center h-10 gap-2">
             <input
               type="text"
               placeholder="Products and categories"
@@ -27,19 +30,13 @@ const Header = () => {
           </div>
           <div className="hidden md:flex md:justify-between md:gap-x-4 text-lg md:items-center">
             <div className=" ">
-              <p className="">
-                Account
-              </p>
+              <p className="">Account</p>
             </div>
             <div className="">
-              <p className="">
-                help
-              </p>
+              <p className="">help</p>
             </div>
             <div className="">
-              <p className="">
-                Cart
-              </p>
+              <p className="">Cart</p>
             </div>
           </div>
         </div>
@@ -48,14 +45,25 @@ const Header = () => {
   );
 };
 
-{/* <PersonIcon className=''/> */}
-{/* <KeyboardArrowDownIcon/> */}
-{/* <HelpIcon className=''/> */}
-{/* <KeyboardArrowDownIcon/> */}
-{/* <ShoppingCartIcon className=''/> */}
-{/* <KeyboardArrowDownIcon/>  */}
+{
+  /* <PersonIcon className=''/> */
+}
+{
+  /* <KeyboardArrowDownIcon/> */
+}
+{
+  /* <HelpIcon className=''/> */
+}
+{
+  /* <KeyboardArrowDownIcon/> */
+}
+{
+  /* <ShoppingCartIcon className=''/> */
+}
+{
+  /* <KeyboardArrowDownIcon/>  */
+}
 
 //           {/* <MenuIcon className='font-black text-2xl md:hidden'/> */}
-
 
 export default Header;
