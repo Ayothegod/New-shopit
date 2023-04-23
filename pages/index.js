546bcde27420563e6f86a9a8b73f3282d001e307
Ayo <heyayomideadebisi@gmail.com>
2023-04-23 17:40:50 +0100

    change bg color of index.js

--- pages/index.js
+++ pages/index.js
@@ -15,11 +15,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-gray-100 h-full py-1 w-screen">
+      <main className="bg-[#e3dddd] min-h-screen w-screen">
         <Header/>
-        <Body/>
-        <p>Hello brother</p>
-        <Footer/>
+        {/* <Body/> */}
+        {/* <Footer/> */}
       </main>
     </>
   );
