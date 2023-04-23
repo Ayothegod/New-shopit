81d137e005fcc16ad0a24b9a6b3fae5b3302ac46
Ayo <heyayomideadebisi@gmail.com>
2023-04-23 21:03:36 +0100

    add modal onclick

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,8 +3,10 @@ import PersonIcon from "@mui/icons-material/Person";
 import HelpIcon from "@mui/icons-material/Help";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { useState } from "react";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="bg-white shadow-lg sticky top-0  ">
       <div className="flex flex-col sm:flex-row sm:justify-between  sm:items-center max-w-[72rem] py-2 px-2 m-auto">
@@ -13,8 +15,26 @@ const Header = () => {
             Shop-It
           </p>
           <div className="flex sm:hidden gap-2 ">
-            <PersonIcon className=" " />
-            <ShoppingCartIcon className="" />
+            <div onClick={() => setOpen(!open)}>
+              <PersonIcon className=" " />
+              {open && (
+                <div className="absolute top-18 py-4 px-6 rounded-md bg-white shadow-lg flex flex-col gap-y-1 w-48 ">
+                  <h1 className="text-[#999]">
+                    Hi{" "}
+                    <span className="text-orange-600 font-semibold text-md ">
+                      user
+                    </span>
+                  </h1>
+                  <p className="text-xs text-[#999]">not signed in? </p>
+                  <button className="text-white bg-orange-600 py-1 px-2 rounded-md border-none">
+                    sign in
+                  </button>
+                </div>
+              )}
+            </div>
+            <div>
+              <ShoppingCartIcon className="" />
+            </div>
           </div>
         </div>
 
@@ -30,20 +50,30 @@ const Header = () => {
             </button>
           </div>
           <div className="hidden md:flex md:justify-between md:gap-x-6 text-lg md:items-center">
-            <div className="flex items-center gap-x-1 relative">
+            <div
+              className="flex items-center gap-x-1 relative cursor-pointer"
+              onClick={() => setOpen(!open)}
+            >
               <PersonIcon className="" />
               <p className="">Account</p>
               <KeyboardArrowDownIcon />
 
-              <div className="absolute top-14 p-4 rounded-md bg-white shadow-lg flex flex-col gap-y-2 ">
-                <h1 className="text-[#999]">
-                  Hi <span className="text-orange-600 font-semibold text-md ">user</span>
-                </h1>
-                <p className="text-xs text-[#999]">not signed in? </p>
-                <button className="text-white bg-orange-600 py-0 px-2 rounded-md border-none">sign in</button>
-              </div>
+              {open && (
+                <div className="absolute top-14 py-4 px-6 rounded-md bg-white shadow-lg flex flex-col gap-y-1 w-48 ">
+                  <h1 className="text-[#999]">
+                    Hi{" "}
+                    <span className="text-orange-600 font-semibold text-md ">
+                      user
+                    </span>
+                  </h1>
+                  <p className="text-xs text-[#999]">not signed in? </p>
+                  <button className="text-white bg-orange-600 py-1 px-2 rounded-md border-none">
+                    sign in
+                  </button>
+                </div>
+              )}
             </div>
-            <div className="flex items-center gap-x-1 ">
+            <div className="flex items-center gap-x-1 cursor-pointer">
               <ShoppingCartIcon className="" />
               <p className="">Cart</p>
             </div>
