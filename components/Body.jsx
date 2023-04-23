55afe853f03697102fcb556358cf13f3826a09f5
Ayo <heyayomideadebisi@gmail.com>
2023-04-23 15:36:32 +0100

    test shop-it-app

--- components/Body.jsx
+++ components/Body.jsx
@@ -7,6 +7,7 @@ const Body = () => {
       Hello, what is going on here
         {/* <Carousel/> */}
         <p>Hello, lets goo to the jungle</p>
+        <p>Lets build Shop-It-App </p>
         <Carousel/>
     </div>
   )
