e009db5681aaeb82b3e6b8459b9489025c7b9545
Ayo <heyayomideadebisi@gmail.com>
2023-04-23 20:04:49 +0100

    solve header input issue

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,40 +7,40 @@
 const Header = () => {
   return (
     <div className="bg-white shadow-lg sticky top-0  ">
-      <div className="flex justify-between py-2 px-4 items-center max-w-[72rem] m-auto">
+      <div className="flex flex-col sm:flex-row sm:justify-between  sm:items-center max-w-[72rem] py-2 m-auto">
         <div className="md:flex items-center ">
-          <p className="text-lg md:text-3xl  font-ptsans font-bold ">
+          <p className="text-lg md:text-3xl md:py-2 px-4 font-ptsans font-bold ">
             Shop-It
           </p>
         </div>
 
-        <div className="flex items-center justify-between gap-4 flex-row-reverse">
-          <div className=" flex justify-between gap-2 items-center">
-            <div className="flex  items-center gap-1 ">
-              <p className="hidden md:flex">
+        <div className="flex items-center  gap-x-4 w-full md:w-4/5 ">
+          <div className=" w-full flex items-center h-10 gap-2 mx-2 ">
+            <input
+              type="text"
+              placeholder="Products and categories"
+              className="border border-neutral-300 rounded-lg p-1 sm:p-2 w-full "
+            />
+            <button className="hidden sm:flex bg-orange-600 text-white font-medium uppercase rounded-md p-2">
+              Search
+            </button>
+          </div>
+          <div className="hidden md:flex md:justify-between md:gap-x-4 text-lg md:items-center">
+            <div className=" ">
+              <p className="">
                 Account
               </p>
             </div>
-            <div className="md:flex  md:items-center gap-1  hidden ">
-              <p className="hidden md:flex">
+            <div className="">
+              <p className="">
                 help
               </p>
             </div>
-            <div className="flex  ">
-              <p className="hidden md:flex">
+            <div className="">
+              <p className="">
                 Cart
               </p>
             </div>
-          </div>
-          <div className="w-full md:w-[40rem] flex items-center h-10 gap-2">
-            <input
-              type="text"
-              placeholder="Search Products, and categories"
-              className="border border-neutral-300 rounded-md p-1 sm:p-2 w-full "
-            />
-            <button className="hidden sm:block bg-orange-600 text-white font-medium uppercase rounded-md p-2">
-              Search
-            </button>
           </div>
         </div>
       </div>
