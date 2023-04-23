7628db2661db74891b7110f8a64f38be4a984949
Ayo <heyayomideadebisi@gmail.com>
2023-04-23 18:11:12 +0100

    remove all grid trash and limit width

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,42 +5,57 @@
 // import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const Header = () => {
-
   return (
-    <div className="bg-white">
-      <div className='grid grid-cols-2 grid-rows-2 md:grid-cols-3 md:grid-rows-1 p-2 items-center'>
-        <div className='flex items-center '>
-          {/* <MenuIcon className='font-black text-2xl md:hidden'/> */}
-          <p className='text-2xl md:text-3xl  font-ptsans font-bold '>Shop-It</p>
+    <div className="bg-white shadow-lg sticky top-0  ">
+      <div className="flex justify-between py-2 px-4 items-center max-w-[72rem] m-auto">
+        <div className="md:flex items-center ">
+          <p className="text-lg md:text-3xl  font-ptsans font-bold ">
+            Shop-It
+          </p>
         </div>
-        <div className='col-span-3 md:col-span-1 flex mr-2 items-center h-10 gap-2 mt-1'>
-          <input type="text" placeholder='Search Products, and categories' className='border border-slate-400 rounded-md p-1 sm:p-2 w-full ' />
-          <button className='hidden sm:block bg-orange-600 text-white font-medium uppercase rounded-md p-2' >Search</button>
-        </div>
-        {/* className='bg-orange-600 text-white font-medium uppercase rounded-md ' */}
-        <div className='col-start-2 col-end-3 row-start-1 row-end-1 md:col-start-3 flex justify-end gap-4 items-center'>
-          <div className='flex ml-8 items-center gap-1 '>
-            {/* <PersonIcon className=''/> */}
-            <p className='hidden md:flex'>Account 
-            {/* <KeyboardArrowDownIcon/> */}
-             </p>
+
+        <div className="flex items-center justify-between gap-4 flex-row-reverse">
+          <div className=" flex justify-between gap-2 items-center">
+            <div className="flex  items-center gap-1 ">
+              <p className="hidden md:flex">
+                Account
+              </p>
+            </div>
+            <div className="md:flex  md:items-center gap-1  hidden ">
+              <p className="hidden md:flex">
+                help
+              </p>
+            </div>
+            <div className="flex  ">
+              <p className="hidden md:flex">
+                Cart
+              </p>
+            </div>
           </div>
-          <div className='md:flex  md:items-center gap-1  hidden '>
-            {/* <HelpIcon className=''/> */}
-            <p className='hidden md:flex'>help 
-            {/* <KeyboardArrowDownIcon/> */}
-             </p>
-          </div>
-          <div className='flex  '>
-            {/* <ShoppingCartIcon className=''/> */}
-            <p className='hidden md:flex'>Cart 
-            {/* <KeyboardArrowDownIcon/>  */}
-            </p>
+          <div className="w-full md:w-[40rem] flex items-center h-10 gap-2">
+            <input
+              type="text"
+              placeholder="Search Products, and categories"
+              className="border border-neutral-300 rounded-md p-1 sm:p-2 w-full "
+            />
+            <button className="hidden sm:block bg-orange-600 text-white font-medium uppercase rounded-md p-2">
+              Search
+            </button>
           </div>
         </div>
       </div>
     </div>
   );
 };
+
+{/* <PersonIcon className=''/> */}
+{/* <KeyboardArrowDownIcon/> */}
+{/* <HelpIcon className=''/> */}
+{/* <KeyboardArrowDownIcon/> */}
+{/* <ShoppingCartIcon className=''/> */}
+{/* <KeyboardArrowDownIcon/>  */}
+
+//           {/* <MenuIcon className='font-black text-2xl md:hidden'/> */}
+
 
 export default Header;
