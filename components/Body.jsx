949a0317cc5f794b760d42bec42975ea040196c5
Ayo <heyayomideadebisi@gmail.com>
2023-04-23 17:38:01 +0100

    add 404 page

--- components/Body.jsx
+++ components/Body.jsx
@@ -4,11 +4,7 @@ import Carousel from "./Carousel"
 const Body = () => {
   return (
     <div>
-      Hello, what is going on here
         {/* <Carousel/> */}
-        <p>Hello, lets goo to the jungle</p>
-        <p>Lets build Shop-It-App </p>
-        <Carousel/>
     </div>
   )
 }
