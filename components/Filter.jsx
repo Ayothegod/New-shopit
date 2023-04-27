03136e5665929f4aea95283571bf15fef468b7f3
Ayo <heyayomideadebisi@gmail.com>
2023-04-27 19:18:12 +0100

    finish filter boiler plate

--- components/Filter.jsx
+++ components/Filter.jsx
@@ -1,7 +1,8 @@
 const Filter = () => {
   return (
     <div className="bg-white">
-      <div className="max-w-[72rem] mx-auto p-1 mt-1 ">
+      <div className="max-w-[72rem] mx-auto pt-1 pb-2 mt-1 ">
+        <p className="font-bold text-xl text-orange-600">Filter </p>
         <div className="grid grid-cols-3 md:grid-cols-6 gap-1">
           <div className="bg-[#ddd6d6] h-16 grid place-items-center rounded-sm w-auto">
             Cheapest
